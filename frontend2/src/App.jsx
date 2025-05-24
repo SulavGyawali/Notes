7db3514661fdeb275818d4e4e9c336e9c19c2f4d
Sulav Gyawali <gyawalisulav7@gmail.com>
2025-05-24 12:56:28 +0545

    added login page

--- frontend2/src/App.jsx
+++ frontend2/src/App.jsx
@@ -1,22 +1,44 @@
-import { useState, useNavigate } from 'react'
-import './App.css'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Home from './components/Home'
-import Login from './components/Login'
-
+import { useState, useEffect } from "react";
+import "./App.css";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useNavigate,
+} from "react-router-dom";
+import Home from "./components/Home";
+import Login from "./components/Login";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const navigate = useNavigate();
 
-  
+  useEffect(() => {
+    // Check if user is logged in
+    const token = localStorage.getItem("token");
+    if (token) {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (!isLoggedIn) {
+      navigate("/login");
+    }
+    else {
+      navigate("/");
+    }
+  }
+  , [isLoggedIn, navigate]);
+ 
+
   return (
-    <Router>
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/login" element={<Login />} />
-      </Routes>
-    </Router>
-  )
+      <Route path="/login" element={<Login setIsLoggedIn={setIsLoggedIn} />} />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
--- frontend2/src/components/Login.jsx
+++ frontend2/src/components/Login.jsx
@@ -1,11 +1,42 @@
-import React from 'react'
-
-const Login = () => {
+import React from "react";
+import { FiUser } from "react-icons/fi";
+import { IoLockClosedOutline } from "react-icons/io5";
+const Login = (props) => {
+  const handleLogin = () => {
+    props.setIsLoggedIn(true);
+  };
   return (
-    <div>
-      
+    <div className="w-screen h-screen flex flex-col gap-10 justify-center items-center text-white">
+      <div className="txt text-6xl custom-kaushan">Nowted</div>
+      <div className="login flex flex-col gap-3">
+        <div className="email w-[20vw] h-[5vh] border flex justify-between items-center">
+          <FiUser className="text-2xl ml-2" />
+          <input
+            type="text"
+            className="w-full h-full focus:outline-0 p-2"
+            placeholder="EMAIL"
+          />
+        </div>
+        <div className="password w-[20vw] h-[5vh]  border flex justify-between items-center">
+          <IoLockClosedOutline className="text-2xl ml-2" />
+          <input
+            type="text"
+            className="w-full h-full focus:outline-0 p-2"
+            placeholder="PASSWORD"
+          />
+        </div>
+      </div>
+      <div className="flex flex-col gap-2">
+        <button
+          className="w-[20vw] h-[5vh] bg-white flex justify-center items-center text-black text-xl font-medium cursor-pointer hover:bg-gray-200"
+          onClick={() => handleLogin()}
+        >
+          <span>LOGIN</span>
+        </button>
+        <span className="flex justify-end opacity-50">Forgot password?</span>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
