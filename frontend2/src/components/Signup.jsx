3432c1627baabd7c90ec66df80a8f84825b4ed7a
Sulav Gyawali <gyawalisulav7@gmail.com>
2025-05-28 20:07:11 +0545

    removed profile picture idea and now working in signup

--- frontend2/src/components/Signup.jsx
+++ frontend2/src/components/Signup.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import {useState} from "react";
 import { FaRegUser } from "react-icons/fa";
 import { MdAlternateEmail } from "react-icons/md";
 import {
@@ -8,19 +8,13 @@ import {
 } from "react-icons/io5";
 
 const Signup = () => {
-  const handleImageUpload = (event) => {
-    const file = event.target.files[0];
-    if (file) {
-      const reader = new FileReader();
-      reader.onloadend = () => {
-        // save the file to ../assets/users_profiles
-        
+  const [showPassword, setShowPassword] = useState(false);
+  const [email, setEmail] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
 
-        console.log("Image uploaded:", reader.result);
-      };
-      reader.readAsDataURL(file);
-    }
-  }
   return (
     <div className="w-screen h-screen flex gap-10 justify-between items-center text-white">
       <div className="txt text-6xl  flex flex-col justify-center items-center w-[40%] h-full bg-white text-black gap-2">
@@ -30,26 +24,7 @@ const Signup = () => {
       <div className="signup h-[90%] w-[45%] flex flex-col  items-center gap-5 mx-auto p-20 pt-10 rounded-2xl">
         <div className="create w-full  flex justify-between items-center mb-5">
           <span className="text-4xl w-full">Create New Account</span>
-          <div className="flex flex-col items-center gap-2">
-            <label
-              htmlFor="image-upload"
-              className="flex flex-col items-center justify-center border-2 border-dashed cursor-pointer bg-gray-50 hover:bg-gray-100 w-[10vw] h-[7vh]"
-            >
-              <p className="flex flex-col justify-center items-center text-sm text-gray-500">
-                <span className="font-semibold">Click to upload</span> or drag and
-                drop
-              </p>
-
-              <input
-                id="image-upload"
-                type="file"
-                className="hidden"
-                accept="image/*"
-                onChange={handleImageUpload}
-              />
-            </label>
-            <span className="text-lg opacity-50">Profile Picture</span>
-          </div>
+         
         </div>
 
         <div className="username flex w-full justify-between items-center gap-[20%]">
@@ -60,6 +35,9 @@ const Signup = () => {
               <input
                 type="text"
                 className="h-full w-full focus:outline-0 text-xl"
+                placeholder="First Name"
+                value={firstName}
+                onChange={(e) => setFirstName(e.target.value)}
               />
             </div>
           </div>
@@ -70,6 +48,9 @@ const Signup = () => {
               <input
                 type="text"
                 className="h-full w-full focus:outline-0 text-xl"
+                placeholder="Last Name"
+                value={lastName}
+                onChange={(e) => setLastName(e.target.value)}
               />
             </div>
           </div>
@@ -81,6 +62,9 @@ const Signup = () => {
             <input
               type="text"
               className="h-full w-full focus:outline-0 text-xl"
+              placeholder="Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
             />
           </div>
         </div>
@@ -89,21 +73,46 @@ const Signup = () => {
           <div className="input w-full border h-[6vh] flex justify-center items-center">
             <IoLockClosedOutline className="text-xl m-2" />
             <input
-              type="text"
+              type={`${showPassword ? "text" : "password"}`}
               className="h-full w-full focus:outline-0 text-xl"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
             />
-            <IoEyeOutline className="text-xl m-2" />
+            {showPassword ? (
+                        <IoEyeOutline
+                          className="text-2xl mr-2 cursor-pointer opacity-50 hover:opacity-100"
+                          onClick={() => setShowPassword(false)}
+                        />
+                      ) : (
+                        <IoEyeOffOutline
+                          className="text-2xl mr-2 cursor-pointer opacity-50 hover:opacity-100"
+                          onClick={() => setShowPassword(true)}
+                        />
+                      )}
           </div>
         </div>
         <div className="confirm flex flex-col gap-2 w-full">
-          <span className="opacity-50">Password</span>
+          <span className="opacity-50">Confirm Password</span>
           <div className="input w-full border h-[6vh] flex justify-center items-center">
             <IoLockClosedOutline className="text-xl m-2" />
             <input
-              type="text"
+              type={`${showPassword ? "text" : "password"}`}
               className="h-full w-full focus:outline-0 text-xl"
+              placeholder="Confirm Password"
+              value={confirmPassword}
             />
-            <IoEyeOutline className="text-xl m-2" />
+            {showPassword ? (
+                        <IoEyeOutline
+                          className="text-2xl mr-2 cursor-pointer opacity-50 hover:opacity-100"
+                          onClick={() => setShowPassword(false)}
+                        />
+                      ) : (
+                        <IoEyeOffOutline
+                          className="text-2xl mr-2 cursor-pointer opacity-50 hover:opacity-100"
+                          onClick={() => setShowPassword(true)}
+                        />
+                      )}
           </div>
         </div>
         <div className="submit">
